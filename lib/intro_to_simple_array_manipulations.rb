f2335b60bef7f7d6b8d789042fbe51ecf45741b6
Ryan Charles Smith <ryan-smith@comcast.net>
2019-11-01 01:47:07 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,43 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end 
+
+def pop_with_args(array)
+  array.pop()
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  array.shift()
+end
+
+def using_insert(array, string)
+  array.insert(string)
+end
+
+def using_uniq(array)
+  array.uniq()
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, int)
+  array.delete(int)
+end
